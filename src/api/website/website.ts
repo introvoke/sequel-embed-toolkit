ec05805ae5124c00a23ae67c4fe6d4d5501cc853
Andrei Ermilov <andrei@introvoke.com>
2024-10-23 16:43:07 -0700

    Added identify endpoints

--- src/api/website/website.ts
+++ src/api/website/website.ts
@@ -12,3 +12,15 @@ export const trackPageView = async (companyId: string, userId: string, sessionId
     const response = await axios.post(configUrl, payload);
     return response;
 };
+
+export const trackIdentify = async (companyId: string, userId: string, sessionId: string, data: any): Promise<AxiosResponse<any, any>> => {
+    const configUrl = `${ApiConfig.GetAnalyticsUrl()}/api/analytics/website/identify`;
+    const payload = {
+        companyId,
+        userId,
+        sessionId,
+        ...data
+    }
+    const response = await axios.post(configUrl, payload);
+    return response;
+};
